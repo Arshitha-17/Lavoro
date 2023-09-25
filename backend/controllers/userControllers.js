1c792ef5cb0ca49baad33a034b854f2f3a6eb3af
Arshitha-17 <arshithaachu165@gmail.com>
2023-09-25 21:27:41 +0530

    password hashed

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -9,8 +9,29 @@ const authUser = asyncHandler(async(req,res)=>{
 // register 
 // route POST /api/users
 const registerUser= asyncHandler(async(req,res)=>{
-    console.log(req.body);
-    res.status(200).json({message:"Register User"})
+    const {name,email,password} = req.body
+    const userExist = await User.findOne({email})
+    if(userExist){
+        res.status(400);
+        throw new Error(`User already exist`)
+    }
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if(user){
+        res.status(201).json({
+            _id:user._id,
+            name:user.name,
+            email:user.email,
+           
+        })
+    }else{
+        res.status(400);
+        throw new Error(`Invalid User data`)
+    }
 })
 
 // logout user
