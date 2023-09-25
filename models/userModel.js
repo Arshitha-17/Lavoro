c080cda19f1b5283db4ae9d543433ed4a065ca56
Arshitha-17 <arshithaachu165@gmail.com>
2023-09-25 20:55:00 +0530

    model created

---
+++ models/userModel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    name:{
+        type:String ,
+        required: true
+    },
+    email:{
+        type:String ,
+        required: true,
+        unique:true
+    },
+    password:{
+        type:String ,
+        required: true
+    },
+    name:{
+        type:String ,
+        required: true
+    },
+
+},{
+    timestamps:true
+})
+
+const User = mongoose.model('User',userSchema);
+export default User;
