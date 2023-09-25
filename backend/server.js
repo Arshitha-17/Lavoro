3cf195798000c2897f407b5b2a1300ea5154cde6
Arshitha-17 <arshithaachu165@gmail.com>
2023-09-25 12:10:03 +0530

    create user route and controllers

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+import express from "express"
+import dotenv from 'dotenv'
+dotenv.config();
+const port = process.env.PORT || 5000
+
+const app =express();
+
+app.get('/',(req,res)=>res.send("server is ready"))
+
+app.listen(port,()=>console.log(`server start port ${port}`))
