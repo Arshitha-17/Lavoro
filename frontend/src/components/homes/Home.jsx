818b67de83d82feaa37326d7b40029b6f9c95c2d
Arshitha-17 <arshithaachu165@gmail.com>
2023-09-27 21:24:43 +0530

    removing unwanted codes

--- frontend/src/components/homes/Home.jsx
+++ frontend/src/components/homes/Home.jsx
@@ -1,9 +1,7 @@
 import { Container, Card, Button } from 'react-bootstrap';
-
 const Home = () => {
   return (
-    <div className=' py-5'>
-          
+    <div className=' py-5'>         
           <div className='d-flex'>
             <Button variant='primary' href='/login' className='me-3'>
               Sign In
