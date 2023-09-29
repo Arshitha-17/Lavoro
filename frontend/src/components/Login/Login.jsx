7946280fc05a6b07a67788e44fb97682199ea39a
Arshitha-17 <arshithaachu165@gmail.com>
2023-09-29 19:10:02 +0530

    fix userInfo erro

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,21 +1,43 @@
-import { useState } from "react";
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from 'react-router-dom'
 import { Col, Form, Row, Button } from 'react-bootstrap'; 
 import FormContainer from "../forms/FormContainer";
 import React from 'react'
 import './Login.css'
 import userProfileImage from '/home/arshithak/Desktop/Brocamp/Week 22/Lavoro/lavoro/frontend/public/Untitled.jpeg'; // Replace with the actual image path
-
+import { usersApi } from "../../axiosApi/axiosInstance";
 
 const Login = () => {
+
+   const userInfo = localStorage.getItem("userInfo")
     const [email,setEmail] = useState('')
     const [password,setPassword] = useState('')
+
+    const navigate = useNavigate();
+
     const submitHandler= async(e)=>{
         e.preventDefault();
         console.log("submit");
+        
+        let formData= {
+          email,password
+        }
+
+        let res = await usersApi.post('users/auth',formData)
+        if(res.data){
+          localStorage.setItem('userInfo',JSON.stringify(res.data))
+          navigate('/')
+        }
+        console.log(res.data);
+        console.log('User logined');
     }
 
-    
+    useEffect(()=>{
+      if(userInfo){
+        navigate('/')
+      }
+    })
+
 
   return (
     <FormContainer  >
