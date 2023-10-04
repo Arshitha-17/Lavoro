690f77590eae2dc2135ad65c8674e78eb1c74969
Arshitha-17 <arshithaachu165@gmail.com>
2023-10-04 14:17:49 +0530

    add headers for each - admin, hr, user

--- frontend/src/components/Lavoro/Lavoro.jsx
+++ frontend/src/components/Lavoro/Lavoro.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
-import Header from '../header/Header'
+import Header from '../header/user/Header'
+import HrHeader from '../header/hr/HrHeader'
+import AdminHeader from '../header/admins/AdminHeader'
+
 import Routers from '../../route/Routers'
+import { useLocation } from 'react-router-dom'
+
+
 const Lavoro = () => {
+const location= useLocation()
+let hr= location.pathname.startsWith('/hr');
+let admin = location.pathname.startsWith('/admin')
   return (
     <div>   
-    <Header/>
+
+ {/* is used to show user to implent error message */}
+
+ {
+          (admin)?<AdminHeader/> :((hr)?<HrHeader /> : <Header />)
+        }
+
+
      <Routers/>
     </div>
   )
---
+++ frontend/src/components/header/admins/AdminHeader.jsx
@@ -0,0 +1,54 @@
+import { Navbar, Nav, Button } from 'react-bootstrap';
+import {FaSignOutAlt } from 'react-icons/fa';
+import logoImage from '/home/arshithak/Desktop/Brocamp/Week 22/Lavoro/lavoro/frontend/public/WhatsApp Image 2023-09-26 at 8.21.43 PM-PhotoRoom.png-PhotoRoom.png'; 
+import './AdminHeader.css'
+import {LinkContainer} from 'react-router-bootstrap'
+const AdminHeader = () => {
+  
+    return (
+      <header>
+      <Navbar className='navbarStyle' variant='dark' expand='lg' collapseOnSelect>
+        <LinkContainer to='/' >
+            <Navbar.Brand >
+              <img style={{ paddingLeft: '15px' }}
+                src={logoImage}
+                alt='Logo'
+                width='150' 
+                height='50'
+                className='d-inline-block align-top' 
+              />
+            </Navbar.Brand>
+        </LinkContainer>
+            <Navbar.Toggle aria-controls='basic-navbar-nav' />
+            <Navbar.Collapse id='basic-navbar-nav' >
+                    <Nav className='ms-auto mx-auto ' > {/* Use mx-auto class for centering */}
+                    <LinkContainer to='/' >
+                    <Nav.Link className=' text-white me-5'><h5>Dashboard</h5></Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to='/jobs'>
+                        <Nav.Link  className=' text-white me-5'><h5>User</h5></Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to='/profile'>
+                        <Nav.Link  className=' text-white me-5'><h5>Hr</h5></Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to='/contact'>
+                        <Nav.Link  className=' text-white me-5'><h5>Contact</h5></Nav.Link>
+                    </LinkContainer>
+                    </Nav>
+                    <LinkContainer to='/login'>
+                    <Nav.Link >
+                      <Button className='me-2 login-button' style={{'borderRadius':'90px' }}>Login</Button>
+                      </Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to='/logout'>
+                    <Nav.Link >
+                        <FaSignOutAlt className='logout' style={{height:'30px'}} />
+                    </Nav.Link>
+                    </LinkContainer>
+            </Navbar.Collapse>
+        </Navbar>
+      </header>
+    );
+};
+
+export default AdminHeader;
