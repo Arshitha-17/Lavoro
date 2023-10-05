9b5f169c2b7a8201f02b49c0deff55af80b44218
Arshitha-17 <arshithaachu165@gmail.com>
2023-10-05 10:36:29 +0530

    otp send successfully

--- backend/controllers/hrControllers.js
+++ backend/controllers/hrControllers.js
@@ -1,35 +1,35 @@
 import asyncHandler from 'express-async-handler'
 import Hr from '../models/hrModel.js'
-import generateToken from '../util/generateToken.js'; 
+import generateToken from '../util/generateToken.js';
+import { HrSendOtpEmail } from './HrSendOtpEmail.js'
 
+const authHr = asyncHandler(async (req, res) => {
 
-const authHr= asyncHandler(async(req,res)=>{
-    console.log('Endhaanu bro ');
-    const {email,password} = req.body;   
-    const  hr = await Hr.findOne({email})   
-    if(hr && (await hr.matchPassword(password))){
-        generateToken(res,hr._id);
+    const { email, password } = req.body;
+    const hr = await Hr.findOne({ email })
+    if (hr && (await hr.matchPassword(password))) {
+        generateToken(res, hr._id);
         res.status(201).json({
-            _id:hr._id,
-            name:hr.name,
-            email:hr.email,           
+            _id: hr._id,
+            name: hr.name,
+            email: hr.email,
         })
-    }else{
+    } else {
         res.status(401);
         throw new Error(`Invalid email or password`)
     }
 });
 
 // hr register 
-// route POST /api/hr
-const HRregister= asyncHandler(async(req,res)=>{
-    const {name,email,password} = req.body
+// route POST /api/hr/register
+const HRregister = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
 
-    const HrExist = await Hr.findOne({email})
-    if(HrExist){
+    const HrExist = await Hr.findOne({ email })
+    if (HrExist) {
         res.status(400);
         throw new Error(`Email already exist`)
-        
+
     }
     const hr = await Hr.create({
         name,
@@ -37,26 +37,73 @@ const HRregister= asyncHandler(async(req,res)=>{
         password
     })
 
-    if(hr){
-        generateToken(res,hr._id);
+    if (hr) {
+        generateToken(res, hr._id);
         res.status(201).json({
-            _id:hr._id,
-            name:hr.name,
-            email:hr.email,
-           
+            _id: hr._id,
+            name: hr.name,
+            email: hr.email,
+
         })
-    }else{
+    } else {
         res.status(400);
         throw new Error(`Invalid User data`)
     }
 })
 
+// hr register 
+// route POST /api/hr/forgot
+
+const HrForgotPassword = asyncHandler(async (req, res) => {
+    const { email } = req.body
+    const hr = await Hr.findOne({email })
+    if (!hr) {
+        return res.status(404).json({ message: 'Email not Found' })
+    }
+    const otp = Math.floor(100000 + Math.random() * 900000);
+    hr.otp = otp;
+
+    await hr.save();
+    const emailSent = await HrSendOtpEmail(hr.email, otp)
+    if (emailSent) {
+        res.status(200).json({ message: 'OTP Send' });
+    } else {
+        res.status(500).json({ message: 'Failed to send email' })
+    }
+
+    // ----------------remove this and uncomment above code 
+    // const emailSent = true
+    // if(emailSent){
+    //         res.status(200).json({ message:'OTP Send'});
+    //     }else{
+    //           res.status(500).json({message:'Failed to send email'})
+    //         }
+})
 
 
 
+// otp verify
+//  route POST  api/hr/otp
+const HrOtpVerify = asyncHandler(async (req, res) => {
+    const { email, otp } = req.body;
+
+    const hr = await Hr.findOne({ email })
+
+    if (hr.otp !== otp) {
+
+        return res.status(400).json({ message: 'Wrong OTP' });
+    } else {
+
+        return res.status(200).json({ hr, message: 'OTP verified successfully' })
+    }
+
+})
 
 
-export{
+
+export {
     authHr,
-    HRregister
+    HRregister,
+    HrForgotPassword,
+    HrOtpVerify
 }
