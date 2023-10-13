82e12dc557f3c3732e11bf169182a168f35591c3
Arshitha-17 <arshithaachu165@gmail.com>
2023-10-13 12:03:21 +0530

    search bar complete

--- frontend/src/components/Users/JobList/JobList.jsx
+++ frontend/src/components/Users/JobList/JobList.jsx
@@ -5,8 +5,18 @@ const JobList = () => {
     <div>
       <div>
         <div className='headings'>
-            <h1 className='mainhead' >Job Listing</h1>
-            <h5 className='subhead' >Delivering Rapid Solutions with Lasting Impression.</h5>
+          <h1 className='mainhead' >Job Listing</h1>
+          <h5 className='subhead' >Delivering Rapid Solutions with Lasting Impression.</h5>
+        </div>
+        <div className='searchbar '>
+          <form className="form-inline my-2 my-lg-0">
+            <div className=" input-group">
+              <input type="text" className="input form-control" placeholder="Your job title or keyword" />
+              <div className="input-group-append">
+                <button className="button btn " type="button">Search</button>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </div>
