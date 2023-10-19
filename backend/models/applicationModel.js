1e7fdc533f3af26751376f500912264ef48bbd2e
Arshitha-17 <arshithaachu165@gmail.com>
2023-10-19 15:30:30 +0530

    application model create

---
+++ backend/models/applicationModel.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const applicationSchema = mongoose.Schema({
+    userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: false,
+        ref:"User"
+    },
+    jobId:{
+        type: mongoose.Schema.Types.ObjectId,
+        default:false,
+        ref:"Job"
+    }
+
+},{
+    timestamps:true
+})
+
+
+const Application = mongoose.model('Application',applicationSchema);
+export default Application;
