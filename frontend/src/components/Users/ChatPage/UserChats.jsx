6b8d52f31430859c8a5d48a169652b2b84552bcb
Arshitha-17 <arshithaachu165@gmail.com>
2023-11-02 09:38:12 +0530

    complete chat UI design

--- frontend/src/components/Users/ChatPage/UserChats.jsx
+++ frontend/src/components/Users/ChatPage/UserChats.jsx
@@ -1,9 +1,100 @@
 import React from 'react'
-
+import "./UserChats.css"
 const UserChats = () => {
   return (
     <div>
-      
+      <div className='chatMainDiv'>
+        <div className='d-flex'>
+          <div className='userListDiv '>
+            <div className='chatSubDiv  m-3'><h5>Zoho Hr</h5></div>
+            <div className='chatSubDiv m-3'><h5>Brototype Hr</h5></div>
+            <div className='chatSubDiv m-3'><h5>TCS Hr</h5></div>
+            <div className=' chatSubDiv m-3'><h5>Bijus Hr</h5></div>
+            <div className=' chatSubDiv m-3'><h5>Bijus Hr</h5></div>
+            <div className=' chatSubDiv m-3'><h5>Bijus Hr</h5></div>
+            <div className=' chatSubDiv m-3'><h5>Bijus Hr</h5></div>
+            <div className=' chatSubDiv m-3'><h5>Bijus Hr</h5></div>
+            <div className=' chatSubDiv m-3'><h5>Bijus Hr</h5></div>
+            <div className=' chatSubDiv m-3'><h5>Bijus Hr</h5></div>
+            <div className=' chatSubDiv m-3'><h5>Bijus Hr</h5></div>
+          </div>
+          <div className='chatDiv'>
+            <div className='nameDiv'> <h4>Arshitha</h4> </div>
+            <div className='chatContainer'>
+              <div className='messageContainerRight my-2'>
+                <div className='messageRight'>
+                  <h6 >Niraj Mannabudhiiiiii</h6>
+                </div>
+              </div>
+              <div className='messageContainerLeft my-2'>
+                <div className='messageLeft'>
+                  <h6 >Niraj Mannabudhiiiiii</h6>
+                </div>
+              </div>
+              <div className='messageContainerRight my-2'>
+                <div className='messageRight'>
+                  <h6 >Niraj Mannabudhiiiiii</h6>
+                </div>
+              </div>
+              <div className='messageContainerLeft my-2'>
+                <div className='messageLeft'>
+                  <h6 >Niraj Mannabudhiiiiii</h6>
+                </div>
+              </div>
+              <div className='messageContainerRight my-2'>
+                <div className='messageRight'>
+                  <h6 >Niraj Mannabudhiiiiii</h6>
+                </div>
+              </div>
+              <div className='messageContainerLeft my-2'>
+                <div className='messageLeft'>
+                  <h6 >Niraj Mannabudhiiiiii</h6>
+                </div>
+              </div>
+              <div className='messageContainerRight my-2'>
+                <div className='messageRight'>
+                  <h6 >Niraj Mannabudhiiiiii</h6>
+                </div>
+              </div>
+              <div className='messageContainerLeft my-2'>
+                <div className='messageLeft'>
+                  <h6 >Niraj Mannabudhiiiiii</h6>
+                </div>
+              </div>
+              <div className='messageContainerRight my-2'>
+                <div className='messageRight'>
+                  <h6 >Niraj Mannabudhiiiiii</h6>
+                </div>
+              </div>
+              <div className='messageContainerLeft my-2'>
+                <div className='messageLeft'>
+                  <h6 >Niraj Mannabudhiiiiii</h6>
+                </div>
+              </div>
+              <div className='messageContainerRight my-2'>
+                <div className='messageRight'>
+                  <h6 >Niraj Mannabudhiiiiii</h6>
+                </div>
+              </div>
+              <div className='messageContainerLeft my-2'>
+                <div className='messageLeft'>
+                  <h6 >Niraj Mannabudhiiiiii</h6>
+                </div>
+              </div>
+            </div>
+            <div className='messageInput'>
+            <form className='messageForm'>
+              <div className='inputMessage'>
+              <input type="text" className='inputBox'/>
+              </div>
+              <div className='submitButtonDiv'>
+              <button type="submit" class="submitButton btn btn-primary">Send</button>
+              </div>
+            </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
