cc2472258bd9ca178312e09a5f59f63261fd4198
Arshitha-17 <arshithaachu165@gmail.com>
2023-11-09 21:03:24 +0530

    backend 5000 port also working in localhost

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express from "express"
 import dotenv from 'dotenv'
 import cors from 'cors'
@@ -30,7 +31,17 @@ app.use('/api/users', userRoutes)
 app.use('/api/hr', hrRoutes)
 app.use('/api/admin', adminRoutes)
 
-app.get('/', (req, res) => res.send("server is ready"))
+if(process.env.NODE_ENV === "production"){
+  const __dirname = path.resolve()
+  app.use(express.static(path.join(__dirname,"frontend/dist")))
+  
+  app.get('*',(req,res)=> res.sendFile(path.resolve(__dirname, 'frontend', 'dist', "index.html")))
+}else{
+
+  app.get('/', (req, res) => res.send("server is ready"))
+}
+
+
 
 app.use(notFound)
 app.use(errorHandler)
