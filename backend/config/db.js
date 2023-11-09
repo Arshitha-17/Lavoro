7e659fff55bcadd481ea70c424229616f99aad9f
Arshitha-17 <arshithaachu165@gmail.com>
2023-11-09 17:50:55 +0530

    db config

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async()=>{
     try {
-        const conn = await mongoose.connect('mongodb+srv://Lavoro:Lavoro123@lavoro.m8wzyey.mongodb.net/Lavoro?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+        const conn = await mongoose.connect('mongodb+srv://Lavoro:Lavoro123@lavoro.m8wzyey.mongodb.net/Lavoro?retryWrites=true', { useNewUrlParser: true, useUnifiedTopology: true });
         
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
