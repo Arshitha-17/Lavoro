11ad462d2764a0b8ea3db6b9be0acc224ca81fb6
Arshitha-17 <arshithaachu165@gmail.com>
2023-11-09 18:16:17 +0530

    edits

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ dotenv.config();
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
-
+ 
 const port = process.env.PORT || 5000
 import userRoutes from "./routes/userRoutes.js"
 import hrRoutes from "./routes/hrRoutes.js"
