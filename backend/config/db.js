14b364e290f3eb3378ff751f53f05a306ec249c6
Arshitha-17 <arshithaachu165@gmail.com>
2023-11-09 15:25:55 +0530

    logo image

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async()=>{
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+        
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         
@@ -11,13 +12,6 @@ const connectDB = async()=>{
 
     }
 }
-// mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => {
-//     console.log('Connected to the database');
-//     // Continue with your server setup or other actions
-//   })
-//   .catch(error => {
-//     console.error('Error connecting to the database:', error.message);
-//   });
+
 
 export default connectDB
