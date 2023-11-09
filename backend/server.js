da68a47bbbe294b5436eaeacf58f586e85454620
Arshitha-17 <arshithaachu165@gmail.com>
2023-11-09 17:35:13 +0530

    server change

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,19 @@
 import express from "express"
 import dotenv from 'dotenv'
 import cors from 'cors'
-dotenv.config();
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
 import path from 'path';
-const port = process.env.PORT || 5000
 import userRoutes from "./routes/userRoutes.js"
 import hrRoutes from "./routes/hrRoutes.js"
 import adminRoutes from './routes/adminRoutes.js'
 
+dotenv.config();
+
+const port = process.env.PORT || 5000
+
+console.log(process.env.NODE_ENV);
 
 connectDB()
 
@@ -32,7 +35,7 @@ app.use('/api/admin', adminRoutes)
 
 
 // production
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV == 'production') {
   const __dirname = path.resolve();
   app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
