4f1dcb79bce67e6e75e14e02e59f61add707b9f7
Arshitha-17 <arshithaachu165@gmail.com>
2023-11-10 15:07:11 +0530

    style

--- frontend/src/components/Users/Contact/Contact.jsx
+++ frontend/src/components/Users/Contact/Contact.jsx
@@ -4,24 +4,25 @@ const Contact = () => {
     return (
         <div>
             <div>
-                <div style={{textAlign:"center",color:"white"}}>
+                <div style={{ textAlign: "center", color: "white" }}>
                     <h4>Get In Touch With Us</h4>
                 </div>
-                <div style={{display:"flex",justifyContent:"space-between"}}>
-                    <div >
+                <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+                    <div style={{textAlign:"center", background:"white"}} >
                         <h6  >Who We Are?</h6>
                         <p>Lavoro stands as India's premier online job portal, catering to the diverse and dynamic employment landscape of the country. As the leading platform connecting talent with opportunities, Lavoro prides itself on its robust and user-friendly interface, offering a comprehensive array of job listings across various industries and career levels. With a commitment to empowering both job seekers and employers, Lavoro harnesses the latest technological advancements to streamline the job search process, providing innovative tools for resume building, job matching, and seamless application processes. Whether you're a seasoned professional or a fresh graduate, Lavoro is dedicated to fostering a thriving ecosystem where talent meets opportunity, ensuring a match made in career heaven.</p>
 
                     </div>
-                    <div >
+                    <div style={{textAlign:"center", background:"white"}} >
                         <h6>For More Information </h6>
-                        <p>Lavoro India Headquarters <br/>
-                            123, Serendipity Avenue,<br/>
-                            Technotropolis, Metro City <br/>
-                            Pin: 560001<br/>
-                            India<br/>
-                            ph:70364201354<br/>
-                            </p>
+                        <p>Advanced Job Search: Tailored search filters for industry, experience level, and location.
+                            Resume Builder: User-friendly tool for crafting professional resumes.
+                            Personalized Job Alerts: Customized notifications for relevant job openings.
+                            Company Profiles: In-depth insights into prospective employers.
+                            Application Tracker: Organized management of submitted applications and their status.
+                            Communication Tools: Seamless interaction between employers and job seekers.
+                            Career Resources: Articles, tips, and guidance for career development and job hunting.
+                        </p>
 
                     </div>
                 </div>
